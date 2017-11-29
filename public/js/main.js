6f8851bf799a548a873c4ae79674f68bcf188166
tzanarde <zanarde.tiago@gmail.com>
2017-11-29 09:39:19 -0200

    Add existing file

---
+++ public/js/main.js
@@ -0,0 +1,124 @@
+var campo = $(".campo-digitacao");
+var tempo_inicial = $("#tempo-digitacao").text();
+
+$(function(){
+    atualizaTamanhoFrase();
+    iniciaContadores();    
+    iniciaCronometro();
+    iniciaMarcadores();
+    $("#botao-reiniciar").click(reiniciaTyper);
+});
+
+function atualizaTamanhoFrase(){
+    var frase = $(".frase").text();
+    var num_palavras = frase.split(" ").length;
+    var tamanho_frase = $("#tamanho-frase");
+    tamanho_frase.text(num_palavras);
+}
+
+function iniciaContadores(){
+    campo.on("input", function(){
+        var conteudo_campo = campo.val();
+
+        var quantidade_palavras = conteudo_campo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(quantidade_palavras);
+
+        var quantidade_caracteres = conteudo_campo.length;
+        $("#contador-caracteres").text(quantidade_caracteres);
+    });
+}
+
+function iniciaCronometro(){
+    var tempo_restante = $("#tempo-digitacao").text();
+    campo.one("focus", function () {
+        $("#botao-reiniciar").attr("disabled", true);
+        var cronometroID = setInterval(function (){
+            tempo_restante--;
+            $("#tempo-digitacao").text(tempo_restante);
+            if(tempo_restante < 1){
+                clearInterval(cronometroID);
+                $("#botao-reiniciar").attr("disabled", false);
+                finalizaTyper();
+            }
+        }, 1000);
+    });
+}
+
+function finalizaTyper(){
+    campo.attr("disabled", true);
+    campo.addClass("campo-desabilitado");
+    adicionaEstatistica();
+}
+
+var frase = 
+campo.on("input", function () {
+    
+});
+
+$("#botao-reiniciar").click(reiniciaTyper);
+
+function reiniciaTyper(){
+    campo.removeClass("campo-desabilitado");
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");    
+
+    $("#tempo-digitacao").text(tempo_inicial);
+    iniciaCronometro();
+
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
+
+}
+
+function iniciaMarcadores(){
+    var frase = $(".frase").text();
+    campo.on("input", function(){
+        var frase_digitada = campo.val();
+        if(frase.startsWith(frase_digitada)){
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        }
+        else{
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+
+    });
+}
+
+function adicionaEstatistica(){
+    var corpo_tabela = $(".estatisticas").find("tbody");
+    var nome_usuario = "Tiago";
+    var qtd_palavras = $("#contador-palavras").text();
+    var linha_tabela = novaLinha(nome_usuario, qtd_palavras);
+    linha_tabela.find(".botao-remover").click(removeLinha);
+
+    corpo_tabela.prepend(linha_tabela);
+}
+
+function novaLinha(pnome_usuario, pqtd_palavras){
+    var linha = $("<tr>");
+    var coluna_nomeusuario = $("<td>").text(pnome_usuario);
+    var coluna_qtdpalavras = $("<td>").text(pqtd_palavras);
+    var coluna_remover = $("<td>");
+    var a = $("<a>").addClass("botao-remover").attr("href", "#");
+    var i = $("<i>").addClass("small").addClass("material-icons").text("delete");
+    
+    a.append(i);
+
+    coluna_remover.append(a);
+
+    linha.append(coluna_nomeusuario);
+    linha.append(coluna_qtdpalavras);
+    linha.append(coluna_remover);
+
+    return linha;
+
+}
+
+function removeLinha(){
+    event.preventDefault();
+    $(this).parent().parent().remove();
+}
